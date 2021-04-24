88212fe8631e8d497e3077d0d6bf63388fd48df6
hy310 <haeyounglee97@gmail.com>
2021-04-24 22:37:00 +0900

    logical operator

---
+++ java/Java_ch02/src/logicalOperator.java
@@ -0,0 +1,19 @@
+
+public class logicalOperator {
+
+	public static void main(String[] args) {
+		
+		System.out.println('a' > 'b');            // false
+		System.out.println(3 >= 2);               // true
+		System.out.println(-1 < 0);               // true
+		System.out.println(3.45 <= 2);            // false
+		System.out.println(3 == 2);               // false
+		System.out.println(3 != 2);               // true
+		System.out.println(!(3 != 2));            // false
+		System.out.println((3>2) && (3>4));       // false
+		System.out.println((3!=2) || (-1>0));     // true
+		System.out.println((3!=2) ^ (-1>0));      // true
+	
+	}
+
+}
