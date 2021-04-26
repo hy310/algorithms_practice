cf551e3c4e5d15b80e4e642d7655659fcc38d75c
hy310 <haeyounglee97@gmail.com>
2021-04-26 23:26:23 +0900

    circle class

--- java/Java_ch04/src/Rectangle.java
+++ java/Java_ch04/src/Rectangle.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
 
 public class Rectangle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
 
 	}
 
