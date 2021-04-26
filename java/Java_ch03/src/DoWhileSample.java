3f8dd60e1ded88f1865719faa9678c5ced2aed13
hy310 <haeyounglee97@gmail.com>
2021-04-26 18:13:25 +0900

    do while

---
+++ java/Java_ch03/src/DoWhileSample.java
@@ -0,0 +1,16 @@
+public class DoWhileSample {
+
+	public static void main(String[] args) {
+		
+		char a = 'a';
+		
+		do
+		{
+			System.out.print(a);
+			a = (char)(a+1);
+			
+		}while(a <= 'z');
+		
+	}
+
+}
