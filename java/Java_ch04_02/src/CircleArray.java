76e76ae35caef1531f2305928d1e2226a646678f
hy310 <haeyounglee97@gmail.com>
2021-04-27 02:12:47 +0900

    circle array

--- java/Java_ch04_02/src/CircleArray.java
+++ java/Java_ch04_02/src/CircleArray.java
@@ -14,10 +14,10 @@ public class CircleArray {
 		Circle [] c;
 		c = new Circle[5];
 		
-		for(int i=0; i<5; i++)
+		for(int i=0; i<c.length; i++)
 			c[i] = new Circle(i);
 		
-		for(int i=0; i<5; i++)
+		for(int i=0; i<c.length; i++)
 			System.out.print((int)(c[i].getArea()) + " " );
 	}
 
