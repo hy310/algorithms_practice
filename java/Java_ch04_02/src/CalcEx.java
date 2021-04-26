b3c55a018ffb0abd592dd7e334d2cb55e6510792
hy310 <haeyounglee97@gmail.com>
2021-04-27 04:33:43 +0900

    CalcEx

--- java/Java_ch04_02/src/CalcEx.java
+++ java/Java_ch04_02/src/CalcEx.java
@@ -6,6 +6,7 @@ class Calc {
 
 public class CalcEx {
 	public static void main(String[] args) {
+		
 		System.out.println(Calc.abs(-5));
 		System.out.println(Calc.max(10, 8));
 		System.out.println(Calc.min(-3, -8));
