1c229528160c69b0e2f91294100d2d4ee0e700f6
hy310 <haeyounglee97@gmail.com>
2021-04-27 11:07:44 +0900

    update

---
+++ java/JavaPractice/src/JavaEx.java
@@ -0,0 +1,13 @@
+
+class JavaEx {
+	int student;
+	
+		public JavaEx(int x) {
+			this.student = x; }
+	
+		public JavaEx() {
+			this(0);
+			System.out.println("생성자 호출"); }
+
+}
+
---
+++ java/JavaPractice/src/operator_equ.java
@@ -0,0 +1,14 @@
+
+public class operator_equ {
+
+	public static void main(String[] args) {
+		int x=2, y=10, z=0;
+//		z = x++ * 2 + --y -5 + x * (y%2);
+		
+		z = ++x * (x+3);
+		System.out.println("z " + z);
+		System.out.println("x " + x);
+
+	}
+
+}
