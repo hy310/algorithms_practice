b427264c4e34b008cf0efc13eb3a6ca34e3c42a5
hy310 <haeyounglee97@gmail.com>
2021-05-29 22:56:39 +0900

    Mouse Adapter

---
+++ java/Java_ch09/src/MouseAdapterEx.java
@@ -0,0 +1,49 @@
+import java.awt.Container;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MouseListenerEx extends JFrame{
+	
+	JLabel la = new JLabel("Hello");
+	
+	public MouseListenerEx() {
+		
+		setTitle("Mouse Event Example");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.addMouseListener(new MyMouseListener());
+		
+		c.setLayout(null);
+		la.setSize(50, 20);
+		la.setLocation(30, 30);
+		c.add(la);
+		
+		setSize(200, 200);
+		setVisible(true);
+		
+	}
+	
+	// 마우스 어댑터를 통해 필요한 것만 사용하기
+	class MyMouseListener extends MouseAdapter {
+		@Override
+		// 이렇게 pressed 하나만 사용가능
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			int x = e.getX();
+			int y = e.getY();
+			la.setLocation(x,y);
+			super.mousePressed(e);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new MouseListenerEx();
+
+	}
+
+}
