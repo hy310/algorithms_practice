2d5cdc60642e150e5bc5080e355f3fae9b5a01c7
hy310 <haeyounglee97@gmail.com>
2021-05-29 22:24:00 +0900

    익명 클래스로 이벤트 리스너 작성

---
+++ java/Java_ch09/src/AnonymousClassListener.java
@@ -0,0 +1,44 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class AnonymousClassListener extends JFrame{
+	public AnonymousClassListener() {
+		setTitle("익명클래스로 Action 이벤트 리스너 만들기");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		JButton btn = new JButton("Action");
+//		btn.addActionListener(new MyActionListener);
+		btn.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JButton b = (JButton)e.getSource();
+				if(b.getText().equals("Action"))
+					b.setText("액션");
+				else
+					b.setText("Action");
+				setTitle(b.getText());
+			}
+		});
+		
+		c.add(btn);
+		
+		setSize(350, 150);
+		setVisible(true);
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new AnonymousClassListener();
+
+	}
+
+}
